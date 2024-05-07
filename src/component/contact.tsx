3b26d6037a92781a3b8d33c10c82fcecf95b9595
Benoit Belloc <belloc.benoitjean@gmail.com>
2024-05-07 17:31:47 +0200

    wip

--- src/component/contact.tsx
+++ src/component/contact.tsx
@@ -1,7 +1,9 @@
-import { Button, Card, CardContent, Divider, Stack, Typography } from "@mui/joy";
-import emailjs, { send } from '@emailjs/browser';
+import { Button, FormControl, FormLabel, Input, Stack, Step, StepButton, StepIndicator, Stepper, Textarea, Typography } from "@mui/joy";
+import emailjs from '@emailjs/browser';
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import React, { useRef } from "react";
+import { Check, CheckCircleIcon, MailIcon } from "lucide-react";
+
 
 export default function Contact() {
     const navigate = useNavigate();
@@ -18,50 +20,61 @@ export default function Contact() {
         })
         .then((result) => {
             console.log(result.text);
+            form.current.reset();
         }, (error) => {
             console.log(error.text);
+            form.current.reset();
         });
     }
 
     return (
         <div>
             <Button onClick={() => navigate('/home')} variant="outlined">Retour</Button>
-            
             <Stack alignItems={"center"} justifyContent="center" mt={3}>
-            <Card
-            variant="outlined"
-            sx={{
-                width: 400,
-                height: 400,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column',
-            }}
-            >
-                <Typography level="title-lg">Contact</Typography>
-                <Divider inset="none" />
-                <CardContent 
-                sx={{
-                    display: 'grid',
-                    gridTemplateColumns: 'repeat(2, minmax(50px, 3fr))',
-                }}>
                     <form ref={form} onSubmit={sendEmail}>
-                        <label>Nom</label>
-                        <input type="text" name="user_lastName" />
-                        <label>Prénom</label>
-                        <input type="text" name="user_firstName" />
-                        <label>Email</label>
-                        <input type="email" name="user_email" />
-                        <label>Objet</label>
-                        <input type="text" name="user_object" />
-                        <label>Message</label>
-                        <textarea name="user_message" />
-                        <Button type="submit">Envoyer</Button>
-            </form>              
-                    </CardContent>
-            </Card>
+                        
+                        <FormControl>
+                        <FormLabel>Nom</FormLabel>
+                        <Input type="text" name="user_lastName" />
+                        </FormControl>
+                        <FormControl>
+                        <FormLabel>Prénom</FormLabel>
+                        <Input type="text" name="user_firstName" />
+                        </FormControl>
+                        <FormControl>
+                        <FormLabel>Email</FormLabel>
+                        <Input type="email" name="user_email" startDecorator={<MailIcon />}/>
+                        </FormControl>
+                        <FormControl>
+                        <FormLabel>Objet</FormLabel>
+                        <Input type="text" name="user_object" />
+                        </FormControl>
+                        <FormControl>
+                        <FormLabel sx={{ gridColumn: '1/-1' }}>Message</FormLabel>
+                        <Textarea required name="user_message" sx={{ height: 100 }}/>
+                        </FormControl>
+                        <Button 
+                        type="submit"
+                        variant="solid"
+                        size="sm"
+                        color="primary"
+                        sx={{ gridColumn: '1/-1', mt: 1}}
+                        // onClick={}
+                        >
+                            Envoyer
+                        </Button>                        
+            </form>           
             </Stack>
         </div>
     )
 }
+
+{/* <Stepper sx={{ width: '100%' }}>
+                            <Step
+                            active
+                            indicator={
+                              <StepIndicator variant="outlined" color="primary">
+                                <CheckCircleIcon />
+                              </StepIndicator>
+                            }><Typography level="title-lg">Identité</Typography></Step>
+                        </Stepper> */}
